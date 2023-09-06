162e9f2a3242b754a784a91b895a10c98379217a
Roman Khabayev <supernaturalmlady@gmail.com>
2023-09-06 18:54:05 +0600

    global app changes. now its trading app

--- main.py
+++ main.py
@@ -1,59 +1,80 @@
-from typing import Optional
+from datetime import datetime
+from enum import Enum
+
 from fastapi import FastAPI
-from fastapi.params import Body
-from pydantic import BaseModel
-from random import randrange
+from pydantic import BaseModel, Field
 
-app = FastAPI()
+from typing import List, Optional
 
+app = FastAPI(
+    title='Trading App'
+)
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    rating: Optional[int] = None
+fake_users = [
+    {'id': 1, 'role': 'admin', 'name': 'Bob'},
+    {'id': 2, 'role': 'investor', 'name': 'John'},
+    {'id': 3, 'role': 'trader', 'name': 'Nick', 'degree': [
+        {'id': 1, 'created_at': '2023-01-01T00:00:00', 'type_degree': 'expert'}
+    ]},
+]
 
+fake_trades = [
+    {'id': 1, 'user_id': 1, 'currency': 'BTC', 'side': 'buy', 'price': 123, 'amount': 2.12},
+    {'id': 2, 'user_id': 2, 'currency': 'BTC', 'side': 'sell', 'price': 125, 'amount': 2.12}
+]
 
-my_posts = [
-    {
-        'title': 'title of post 1',
-        'content': 'content of post 1',
-        'id': 1
-    },
-    {
-        'title': 'favorite food',
-        'content': 'pizza',
-        'id': 2
-    }
-
+fake_users2 = [
+    {'id': 1, 'role': 'admin', 'name': 'Bob'},
+    {'id': 2, 'role': 'investor', 'name': 'John'},
+    {'id': 3, 'role': 'trader', 'name': 'Nick'},
 ]
 
 
-def find_post(id):
-    for p in my_posts:
-        if p['id'] == id:
-            return p
+class DegreeType(Enum):
+    newbie = 'newbie'
+    expert = 'expert'
 
 
-@app.get('/posts')
-def get_posts():
-    return {'data': my_posts}
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
 
 
-@app.get('/')
-def root():
-    return {'message': 'Welcome to my first API'}
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]] = []
 
 
-@app.post('/posts')
-def create_post(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+@app.get('/users/{user_id}', response_model=List[User])
+def get_user(user_id: int):
+    return [user for user in fake_users if user.get('id') == user_id]
 
 
-@app.get('/posts/{id}')
-def get_post(id: int):
-    post = find_post(id)
-    return {'post_detail': post}
+@app.get('/trades')
+def get_trades(limit: int = 1, offset: int = 0):
+    return fake_trades[offset:][:limit]
+
+
+@app.post('/users/{user_id}')
+def change_name(user_id: int, new_name: str):
+    current_user = list(filter(lambda user: user.get('id') == user_id, fake_users2))[0]
+    current_user['name'] = new_name
+    return {'status': 200, 'data': current_user}
+
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float = Field(ge=0)
+
+
+@app.post('/trades')
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {'status': 200, 'data': fake_trades}
